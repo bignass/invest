29de9e5784e5aac08639155b9dd516554529d276
bignass <61742780+bignass@users.noreply.github.com>
2020-03-03 18:29:56 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    MoreThanBillion.com
+                    MoreThanBillion
                 </div>
 
                 <div class="links">
