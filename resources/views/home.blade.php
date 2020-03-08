5da5fee6cae7e0dde34c559ee601236d4e147443
bignas <52691966+bignas@users.noreply.github.com>
2020-03-08 22:55:35 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-12 text-center img-fluid "><img src="{{asset('storage/uploads/money_pile_dribble_1.gif')}}" alt=""></div>
+        <div class="col-12 text-center .img-fluid. max-width: 100%;"><img src="{{asset('storage/uploads/money_pile_dribble_1.gif')}}" alt=""></div>
     </div>
 </div>
 @endsection
