09898cd1bca58c63a34e553b4763b155d4f076c0
bignas <52691966+bignas@users.noreply.github.com>
2020-03-08 21:15:37 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
   return view('home');
 });
 
+Route::get('/dashboard', function () {
+  return view('dashboard');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
