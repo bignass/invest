4985f7a36f777b997068e81e890983b48df1496f
bignas <52691966+bignas@users.noreply.github.com>
2020-03-14 14:19:01 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,8 +55,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
                                     <a class="dropdown-item" href="{{ url('dashboard') }}"">Dashboard</a>
+
+
+                                    <a class="dropdown-item" href="{{ route('user.edit')}}"">
+                                        {{__('Edit Details')}}
+                                    </a>
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
