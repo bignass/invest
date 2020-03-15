c61f3938a52f569bff3125998863c3f91a1c7c32
bignas <52691966+bignas@users.noreply.github.com>
2020-03-15 16:02:29 +0200

    Update edit.blade.php

--- resources/views/pages/user/edit.blade.php
+++ resources/views/pages/user/edit.blade.php
@@ -30,6 +30,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
+
+                            <div class="col-md-6">
+                            <input id="last_name" value="{{$user['last_name']}}" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name">
+
+                                @error('last_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
