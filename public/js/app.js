caf28ef817ad10480887fdd0402fc32687927bcf
bignas <52691966+bignas@users.noreply.github.com>
2020-03-16 15:24:29 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/invest/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/invest/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Ignas Buinickas\Desktop\KTU\K2S2\Semestro projektas\Invest\invest\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Ignas Buinickas\Desktop\KTU\K2S2\Semestro projektas\Invest\invest\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
