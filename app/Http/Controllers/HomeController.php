83b686e890db892ea9dc5c84721aef8f8af84681
bignas <52691966+bignas@users.noreply.github.com>
2020-03-17 12:34:46 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
    */
   public function __construct()
   {
-    $this->middleware(['auth', 'verified']);
+    //$this->middleware(['auth', 'verified']);
   }
 
   /**
@@ -23,6 +25,10 @@ class HomeController extends Controller
    */
   public function index()
   {
-    return view('home');
+    if (Auth::user()) {
+      return view('dashboard');
+    } else {
+      return view('pages.home.home');
+    }
   }
 }
